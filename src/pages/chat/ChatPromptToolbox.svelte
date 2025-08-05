b253c636cbf925b83fac6596d08ef8b13b017f6f
Putra Budiman <cristminix@gmail.com>
2025-08-05 23:47:14 +0700

    ...

--- src/pages/chat/ChatPromptToolbox.svelte
+++ src/pages/chat/ChatPromptToolbox.svelte
@@ -1,12 +1,20 @@
-<label class="font-bold pb-8">Available Tools:</label>
-<div class="flex">
-  <input
-    type="checkbox"
-    class="shrink-0 mt-0.5 border-gray-200 rounded-sm text-blue-600 focus:ring-blue-500 checked:border-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-800 dark:border-neutral-700 dark:checked:bg-blue-500 dark:checked:border-blue-500 dark:focus:ring-offset-gray-800"
-    id="hs-default-checkbox"
-  />
-  <label
-    for="hs-default-checkbox"
-    class="text-sm text-gray-500 ms-3 dark:text-neutral-400">Time</label
-  >
+<script lang="ts">
+  let timeToolEnabled = false;
+</script>
+
+<label class="font-bold mb-4 block">Available Tools:</label>
+<div class="toolbox-list">
+  <div class="flex">
+    <input
+      bind:checked={timeToolEnabled}
+      type="checkbox"
+      class="shrink-0 mt-0.5 border-gray-200 rounded-sm text-blue-600 focus:ring-blue-500 checked:border-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-800 dark:border-neutral-700 dark:checked:bg-blue-500 dark:checked:border-blue-500 dark:focus:ring-offset-gray-800"
+      id="hs-default-checkbox"
+    />
+    <label
+      for="hs-default-checkbox"
+      class="text-sm text-gray-500 ms-3 dark:text-neutral-400"
+      >Time : <code>getCurrentTime</code></label
+    >
+  </div>
 </div>
--- src/pages/chat/ChatStreaming.svelte
+++ src/pages/chat/ChatStreaming.svelte
@@ -1,27 +1,29 @@
 <script lang="ts">
-  import UserMessage from "./UserMessage.svelte"
-  import AssistantMessage from "./AssistantMessage.svelte"
-  import { onMount } from "svelte"
-  import { autoScroll } from "./chat-page/fn/autoScroll"
-  import type { ChatMessageInterface } from "./chat-page/types"
-  export let onProcessingDone: (text: string, id: number | string) => void
-  export let messages: ChatMessageInterface[] = []
-  export let model: string = "gpt-4:blackbox"
-  export let conversation_id: string
-  export let provider: string
-  export let messageId: string | number
+  import UserMessage from "./UserMessage.svelte";
+  import AssistantMessage from "./AssistantMessage.svelte";
+  import { onMount } from "svelte";
+  import { autoScroll } from "./chat-page/fn/autoScroll";
+  import type { ChatMessageInterface } from "./chat-page/types";
+  export let onProcessingDone: (text: string, id: number | string) => void;
+  export let messages: ChatMessageInterface[] = [];
+  export let model: string = "gpt-4:blackbox";
+  export let conversation_id: string;
+  export let provider: string;
+  export let messageId: string | number;
 
-  let chatContainer
-
+  let chatContainer;
+  let filteredMessages = messages.filter((m) => m.role !== "system");
   onMount(() => {
-    chatContainer = document.querySelector("#chat-container")
-    autoScroll()
-  })
+    chatContainer = document.querySelector("#chat-container");
+    autoScroll();
+  });
 </script>
 
 <ul id="chat-container" class="mt-16 space-y-5 conversation-list">
   {#if messages.length > 0}
-    <UserMessage content={messages[messages.length - 1].content} />
+    <UserMessage
+      content={filteredMessages[filteredMessages.length - 1].content}
+    />
     <AssistantMessage
       {onProcessingDone}
       isStreaming={true}
