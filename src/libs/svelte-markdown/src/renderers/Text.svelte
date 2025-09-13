635c9c8f45123af40323c1f872b1e902e000abf0
Putra Budiman <cristminix@gmail.com>
2025-09-13 20:46:24 +0700

    ...

--- src/libs/svelte-markdown/src/renderers/Text.svelte
+++ src/libs/svelte-markdown/src/renderers/Text.svelte
@@ -2,8 +2,8 @@
   export let text
   export let raw
   let match = false
-  if (text.match(/\[\d+\]\s*|\b\d+\b/g)) {
-    text = text.replace(/\[(\d+)\]\s*|\b\d+\b/g, "$1. ")
+  if (text.match(/\[\d+\]\s{1}/g)) {
+    text = text.replace(/\[(\d+)\]\s{1}/g, "$1. ")
     match = true
   }
   // console.log({ text })
